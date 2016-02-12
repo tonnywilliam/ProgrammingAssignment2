70654ce6e8a2f9f5e4fc5e46f5034a1cb7e8ed72
tonnywilliam <dropboxhx@gmail.com>
2016-02-12 13:09:49 -0600

    use the assignment template, wrote functions to calculate and cache the inverse of a matrix
    using these functions, you only need to calculate the inverse of a matrix once.
    next time when you need the inverse, you can retrieve it from cache
    this could save a lot of re-calculating the inverse of a maatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## create a mechanism to cache the inverse of matrices, so that you do not need to perform computation intensive matrix inversing operations repeatedly on the same matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix defines the method to cache the inverse of a matrix (through setinverse), together with three other functions
+## it sort of create a class CacheMatrix with four methods
+## set/get/setinverse/getinverse
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL	# m is like a global variable in the makeCacheMatrix function scope
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## this function takes input x which is created by the makeCacheMatrix, it checks wether its inverse has being cached. if yes then it return the cached inverse, if not then it calculate the inverse and cache it, it also returns it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)		#this is where the cache happens
+        m
 }
